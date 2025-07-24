4315b696f88c2f7e845fce1837bae31a4859e805
laurijssen <49683709+laurijssen@users.noreply.github.com>
2025-07-24 15:50:12 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "cacerts-webhook/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ root.go
@@ -0,0 +1,198 @@
+package cmd
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+	admissionv1 "k8s.io/api/admission/v1"
+	v1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var (
+	tlsCert string
+	tlsKey  string
+	port    int
+	codecs  = serializer.NewCodecFactory(runtime.NewScheme())
+	logger  = log.New(os.Stdout, "http: ", log.LstdFlags)
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cacerts-webhook",
+	Short: "Kubernetes webhook to call update-ca-certs on every pod creation",
+	Long: `Kubernetes webhook to call update-ca-certs on every pod creation.
+
+Example:
+$ cacerts-webhook --tls-cert <tls_cert> --tls-key <tls_key> --port <port>`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if tlsCert == "" || tlsKey == "" {
+			fmt.Println("--tls-cert and --tls-key required")
+			os.Exit(1)
+		}
+		runWebhookServer(tlsCert, tlsKey)
+	},
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.Flags().StringVar(&tlsCert, "tls-cert", "", "Certificate for TLS")
+	rootCmd.Flags().StringVar(&tlsKey, "tls-key", "", "Private key file for TLS")
+	rootCmd.Flags().IntVar(&port, "port", 443, "Port to listen on for HTTPS traffic")
+}
+
+func admissionReviewFromRequest(r *http.Request, deserializer runtime.Decoder) (*admissionv1.AdmissionReview, error) {
+	if r.Header.Get("Content-Type") != "application/json" {
+		return nil, fmt.Errorf("expect application/json content-type")
+	}
+
+	var body []byte
+	if r.Body != nil {
+		requestData, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		body = requestData
+	}
+
+	admissionReviewRequest := &admissionv1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, admissionReviewRequest); err != nil {
+		return nil, err
+	}
+
+	return admissionReviewRequest, nil
+}
+
+func mutatePod(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("received message on mutate")
+
+	deserializer := codecs.UniversalDeserializer()
+
+	admissionReviewRequest, err := admissionReviewFromRequest(r, deserializer)
+	if err != nil {
+		msg := fmt.Sprintf("error getting admission review from request: %v", err)
+		logger.Printf(msg)
+		w.WriteHeader(400)
+		w.Write([]byte(msg))
+		return
+	}
+
+	podResource := metav1.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
+	if admissionReviewRequest.Request.Resource != podResource {
+		msg := fmt.Sprintf("did not receive pod, got %s", admissionReviewRequest.Request.Resource.Resource)
+		logger.Printf(msg)
+		w.WriteHeader(400)
+		w.Write([]byte(msg))
+		return
+	}
+
+	rawRequest := admissionReviewRequest.Request.Object.Raw
+	pod := corev1.Pod{}
+	if _, _, err := deserializer.Decode(rawRequest, nil, &pod); err != nil {
+		msg := fmt.Sprintf("error decoding raw pod: %v", err)
+		logger.Printf(msg)
+		w.WriteHeader(500)
+		w.Write([]byte(msg))
+		return
+	}
+
+	admissionResponse := &admissionv1.AdmissionResponse{}
+	var patch string
+	patchType := v1.PatchTypeJSONPatch
+
+	patch = `[
+	{
+	"op": "add",
+	"path": "/spec/initContainers",
+	"value": []
+	},
+	{
+		"op": "add",
+		"path": "/spec/initContainers/-",
+		"value": {
+		"name": "certificates-to-shared-volume",
+		"image": "ubuntu",
+		"command": ["/bin/sh", "-c"],
+		"args": [
+			"update-ca-certificates && cp -r /etc/ssl/certs/* /certs-out/"
+		],
+		"volumeMounts": [
+			{
+			"name": "certs-out",
+			"mountPath": "/certs-out"
+			}
+		]
+		}
+	},
+	{
+		"op": "add",
+		"path": "/spec/volumes/-",
+		"value": {
+		"name": "certs-out",
+		"emptyDir": {}
+		}
+	},
+	{
+		"op": "add",
+		"path": "/spec/containers/0/volumeMounts/-",
+		"value": {
+		"name": "certs-out",
+		"mountPath": "/etc/ssl/certs"
+		}
+	}
+	]`		
+
+	admissionResponse.Allowed = true
+	admissionResponse.PatchType = &patchType
+	admissionResponse.Patch = []byte(patch)
+
+	// Construct the response, which is just another AdmissionReview.
+	var admissionReviewResponse admissionv1.AdmissionReview
+	admissionReviewResponse.Response = admissionResponse
+	admissionReviewResponse.SetGroupVersionKind(admissionReviewRequest.GroupVersionKind())
+	admissionReviewResponse.Response.UID = admissionReviewRequest.Request.UID
+
+	resp, err := json.Marshal(admissionReviewResponse)
+	if err != nil {
+		msg := fmt.Sprintf("error marshalling response json: %v", err)
+		logger.Printf(msg)
+		w.WriteHeader(500)
+		w.Write([]byte(msg))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func runWebhookServer(certFile, keyFile string) {
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("starting webhook on port %d\n", port)
+	http.HandleFunc("/mutate", mutatePod)
+	server := http.Server{
+		Addr: fmt.Sprintf(":%d", port),
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		},
+		ErrorLog: logger,
+	}
+
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+		panic(err)
+	}
+}
